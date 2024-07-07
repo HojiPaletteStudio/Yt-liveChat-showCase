38f7cdaab1783023c07a9e747f72fc4e611dc3a4
Hoji Palette Studio <161171919+HojiPaletteStudio@users.noreply.github.com>
2024-07-07 16:36:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+// export let imgUser = `./assets/HojiIcon.png`;
+// export let emoji =``;
+// export let domImg = `./assets/HandCatdom.png`;
+// export let ModImg = `./assets/yourStream.png`;
+// export let setNameViewer = [
+//     "../../assets/img1.jpg",
+//     "../../assets/img2.jpg",
+//     "../../assets/img3.jpg",
+//     "../../assets/img4.jpg"
+// ];
+// export let setViewerImg = [
+//     "../../assets/viewer/viewer1.png",
+//     "../../assets/viewer/viewer2.png",
+//     "../../assets/viewer/viewer3.png",
+//     "../../assets/viewer/viewer4.png",
+//     "../../assets/viewer/viewer5.png",
+//     "../../assets/viewer/viewer6.png",
+//     "../../assets/viewer/viewer7.png",
+//     "../../assets/viewer/viewer8.png",
+//     "../../assets/viewer/viewer9.png",
+//     "../../assets/viewer/viewer10.png",
+//     "../../assets/viewer/viewer11.png",
+//     "../../assets/viewer/viewer12.png",
+//     "../../assets/viewer/viewer13.png"
+// ];
+// export let setMemberImg = [
+//     "../../assets/member/mem1.png",
+//     "../../assets/member/mem2.png",
+//     "../../assets/member/mem3.png",
+//     "../../assets/member/mem4.png",
+//     "../../assets/member/mem5.png",
+//     "../../assets/member/mem6.png"
+// ];
+// export let setMemberName = [
+//     "I'm-member-one",
+//     "I'm-member-two",
+//     "I'm-member-three",
+//     "I'm-member-four",
+//     "I'm-member-five",
+//     "I'm-member-six",
+// ]
+
 export let imgUser = `./assets/HojiIcon.png`;
 export let emoji =``;
 export let domImg = `./assets/HandCatdom.png`;
@@ -9,27 +51,27 @@ export let setNameViewer = [
     "../../assets/img4.jpg"
 ];
 export let setViewerImg = [
-    "../../assets/viewer/viewer1.png",
-    "../../assets/viewer/viewer2.png",
-    "../../assets/viewer/viewer3.png",
-    "../../assets/viewer/viewer4.png",
-    "../../assets/viewer/viewer5.png",
-    "../../assets/viewer/viewer6.png",
-    "../../assets/viewer/viewer7.png",
-    "../../assets/viewer/viewer8.png",
-    "../../assets/viewer/viewer9.png",
-    "../../assets/viewer/viewer10.png",
-    "../../assets/viewer/viewer11.png",
-    "../../assets/viewer/viewer12.png",
-    "../../assets/viewer/viewer13.png"
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer1.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer2.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer3.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer4.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer5.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer6.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer7.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer8.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer9.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer10.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer11.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer12.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/viewer/viewer13.png?raw=true"
 ];
 export let setMemberImg = [
-    "../../assets/member/mem1.png",
-    "../../assets/member/mem2.png",
-    "../../assets/member/mem3.png",
-    "../../assets/member/mem4.png",
-    "../../assets/member/mem5.png",
-    "../../assets/member/mem6.png"
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/member/mem1.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/member/mem2.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/member/mem3.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/member/mem4.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/member/mem5.png?raw=true",
+    "https://github.com/HojiPaletteStudio/Yt-liveChat-showCase/blob/main/assets/member/mem6.png?raw=true"
 ];
 export let setMemberName = [
     "I'm-member-one",
